a73c72fd923f23e8ba750f741514edfd10842773
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-04-16 20:48:47 -0500

    working on the output for index

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var unpassword = []
+let unpassword = []
 let megaArray = []
-const charaSet1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let numberSet = ["1","2","3","4","5","6","7","8","9","0"];
-var charaSet2 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-charaSet2 = charaSet2.map(function(x){return x.toUpperCase();})
+const charaSet1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numberSet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var charaSet2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+charaSet2 = charaSet2.map(function (x) { return x.toUpperCase(); })
 
 
 
@@ -14,60 +14,89 @@ charaSet2 = charaSet2.map(function(x){return x.toUpperCase();})
 
 
 
-function generatePassword() 
-{
-
-
-let numChar = prompt("How many characters would you like to use?")
-
-if (numChar < 8) {
-  console.log("I am sorry that is an invlaid selection");
-alert("That is an invlaid selection, please try again");
-
-} else if (numChar > 128) {
-   
-console.log("I am sorry that is an invlaid selection");
-alert("That is an invlaid selection, please try again")
-}
-
-else { console.log("That is great! We can do " + numChar +" characters") }
-
-
-let Uppercase = prompt ("Do you want UPPER CASE letters(y/n)")
- 
-if (Uppercase == "y") {const ranUp = Math.floor(Math.random() * 26) 
-  unpassword.push(charaSet2[ranUp]);
-  megaArray.push(charaSet2)}
+function generatePassword() {
   
-  else {console.log ("Uppercase letters denied")}
+  const numChar = +prompt("How many characters would you like to use?")
 
-let Lowercase = prompt ("Do you want lower case letters(y/n)")
+  if (numChar < 8) {
+    console.log("I am sorry that is an invlaid selection");
+    alert("That is an invlaid selection, please try again");
 
-if  (Lowercase == "y") {
+  } else if (numChar > 128) {
+
+    console.log("I am sorry that is an invlaid selection");
+    alert("That is an invlaid selection, please try again")
+  }
+
+  else { console.log("That is great! We can do " + numChar + " characters") } 
+console.log(numChar)
+const numChar2 = new Array(numChar);
+console.log(numChar2.length)
+  let Uppercase = prompt("Do you want UPPER CASE letters(y/n)")
+
+  if (Uppercase == "y") {
+    const ranUp = Math.floor(Math.random() * 26)
+    unpassword.push(charaSet2[ranUp]);
+    megaArray.push(charaSet2)
+  }
+
+
+  else if (Uppercase == "n") {
+    console.log("Uppercase letters denied")
+  }
+
+  else { alert("that is an invalid selection"); return;}
+
+
+let Lowercase = prompt("Do you want lower case letters(y/n)")
+
+if (Lowercase == "y") {
   const ranLow = Math.floor(Math.random() * 26)
   unpassword.push(charaSet1[ranLow]);
-  megaArray.push(charaSet1) }
-else {console.log("lowercase letters denied") }
+  megaArray.push(charaSet1)
+}
+
+else if (Lowercase == "n") { console.log("lowercase letters denied") }
+
+else { alert("that is an invalid selection"); return; }
 
 
 
 let numberS = prompt("Do you want numbers in this password?(y/n)")
 
-if (numberS == "y") {const ranNum = Math.floor(Math.random() * 10)
+if (numberS == "y") {
+  const ranNum = Math.floor(Math.random() * 10)
   unpassword.push(numberSet[ranNum]);
-  megaArray.push(numberSet)}
-else {console.log("numbers denied")}
+  megaArray.push(numberSet)
+} else if (numberS == "n") { console.log("numbers denied") 
 
-let  (unpassword.length -1) === numChar { 
-  for (let i = unpassword.length; i < numChar.length; i++){
-  const ranChar =Math.floor(Math.Random() * numChar.length)
-  unpassword.push(megaArray)}}
+}else { alert ("That is an invalid selection") }
+
+
+confirm("are you sure you want these parameters?, password lenght: " + numChar +", Character set uppercase: " + Uppercase +", Character set lowercase: " + Lowercase +", Number set: " + numberS)
+
+console.log(megaArray);
+
+if (megaArray.length <= numberS.length) 
+ { alert("you have not selected a character set for this password") }
+
+  
+  for (var i = unpassword.length; i < numChar2.length; i++){
+
+ const ranVal = [Math.floor(Math.random() * megaArray.length)];
+ const ranVal2 = [Math.floor(Math.random() * ranVal.length)];
+
+  unpassword.push(ranVal[ranVal2])} 
+
 console.log(unpassword);
+return 
+}
 
- 
-console.log (unpassword);}
 
 
+
+
+console.log(unpassword);
 
 
 
@@ -86,14 +115,14 @@ console.log (unpassword);}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 
 }
 
-  
 
-    
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
