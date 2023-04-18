c19b2e323af83a1c0cb0af02f3f26077d8d9c858
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-04-17 21:47:21 -0500

    added functionallity to the script and turned Prompts into confirms

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Password-Gen-code
+
+## Description
+    I coded the JavaScript of this Password Generator to push myself in my understanding of JavaScript.
+
+## Instalation
+    N/A
+
+## Usage
+    This code is ment to be used to create safe and secure passwords with random characters.
+
+## Credits
+    Starter code courtesy of Columbia Coding Bootcamp
+
+    
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let unpassword = []
+
 let megaArray = []
 const charaSet1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let numberSet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -15,26 +15,27 @@ charaSet2 = charaSet2.map(function (x) { return x.toUpperCase(); })
 
 
 function generatePassword() {
-  
+  let unpassword = []
   const numChar = +prompt("How many characters would you like to use?")
 
   if (numChar < 8) {
     console.log("I am sorry that is an invlaid selection");
     alert("That is an invlaid selection, please try again");
+    return
 
   } else if (numChar > 128) {
 
     console.log("I am sorry that is an invlaid selection");
-    alert("That is an invlaid selection, please try again")
+    alert("That is an invlaid selection, please try again");
+    return
+  
   }
 
   else { console.log("That is great! We can do " + numChar + " characters") } 
 console.log(numChar)
-const numChar2 = new Array(numChar);
-console.log(numChar2.length)
-  let Uppercase = prompt("Do you want UPPER CASE letters(y/n)")
+  let Uppercase = confirm("Do you want UPPER CASE letters(okay/cancel)")
 
-  if (Uppercase == "y") {
+  if (Uppercase) {
     const ranUp = Math.floor(Math.random() * 26)
     unpassword.push(charaSet2[ranUp]);
     const rat=[];
@@ -42,57 +43,53 @@ console.log(numChar2.length)
   }
 
 
-  else if (Uppercase == "n") {
+  else if (!Uppercase) {
     console.log("Uppercase letters denied")
   }
 
   else { alert("that is an invalid selection"); return;}
 
 
-let Lowercase = prompt("Do you want lower case letters(y/n)")
+let Lowercase = confirm("Do you want lower case letters(okay/cancel)")
 
-if (Lowercase == "y") {
+if (Lowercase) {
   const ranLow = Math.floor(Math.random() * 26)
   unpassword.push(charaSet1[ranLow]);
   var measure = bar.concat(charaSet1);
 }
 
-else if (Lowercase == "n") { console.log("lowercase letters denied") }
+else if (!Lowercase) { console.log("lowercase letters denied") }
 
 else { alert("that is an invalid selection"); return; }
 
 
 
-let numberS = prompt("Do you want numbers in this password?(y/n)")
+let numberS = confirm("Do you want numbers in this password?(okay/cancel)")
 
-if (numberS == "y") {
+if (numberS) {
   const ranNum = Math.floor(Math.random() * 10)
   unpassword.push(numberSet[ranNum]);
   var megaArray= measure.concat(numberSet)
-} else if (numberS == "n") { console.log("numbers denied") 
+} else if (!numberS) { console.log("numbers denied") 
 
 }else { alert ("That is an invalid selection") }
 
 
-confirm("are you sure you want these parameters?, password lenght: " + numChar +", Character set uppercase: " + Uppercase +", Character set lowercase: " + Lowercase +", Number set: " + numberS)
+confirm("are you sure you want these parameters?\n password lenght: " + numChar +"\n Character set uppercase: " + Uppercase +"\n Character set lowercase: " + Lowercase +"\n Number set: " + numberS)
 
-console.log(megaArray);
 
 if (megaArray.length <= numberS.length) 
  { alert("you have not selected a character set for this password") }
+  let  middle = []
+  for (var i = unpassword.length; i < numChar; i++){
 
-  
-  for (var i = unpassword.length; i < numChar2.length; i++){
-
- let  reVal = Math.floor(Math.random() * megaArray.length);
+ let  reVal = Math.floor(Math.random() * megaArray.length-1);
  
- const middle = []
- middle.push(reVal)
- middle[Math.floor(Math.random() * reVal.length)]
- console.log(middle)}
-console.log(unpassword)
-
- return 
+ 
+ middle.push(megaArray[reVal])
+ middle[Math.floor(Math.random() * reVal.length)] }
+unpassword=unpassword.concat(middle).join("")
+return unpassword 
   }
 
 
